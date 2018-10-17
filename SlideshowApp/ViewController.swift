7a3558d2d14c711bc2caac3d6532b92847377e26
yutaka.tanabe <toncya.tanabe@gmail.com>
2018-10-17 15:01:10 +0900

    うまく動いていないが、変更箇所把握のためにコミット

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,17 +9,78 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var imageview: UIImageView!
+    var imageArray = ["slide01", "slide02", "slide03", "slide04"]
+    var index = 0
+    
+    var flg = false
+    
+    @IBAction func onTapImage(_ sender: Any) {
+        
+        // segueを使用して画面遷移
+        performSegue(withIdentifier: "result", sender: nil)
+    }
+    
+    // すすむボタンの動作を設定
+    @IBAction func susumu(_ sender: UIButton) {
+        index += 1
+        displayImage()
+    }
+    func displayImage() {
+        if index >= imageArray.count {
+            index = 0
+        }
+        if index <= 0 {
+            index = 3
+        }
+        imageview.image = UIImage(named: imageArray[index])
+    }
+    
+    // もどるボタンの動作を設定
+    @IBAction func modoru(_ sender: UIButton) {
+        index += -1
+        displayImage()
+    }
+    
+    // スライドショー
+    var timer: Timer!
+    var timer_sec: Float = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        displayImage()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    //タイマーを2秒ごとに進める
+    @objc func updateTimer(_ timer: Timer) {
+        self.timer_sec += 2.0
+        index += 1
+        displayImage()
+    }
+    
+    //再生ボタンを押すとスライドショースタート
+    @IBAction func startSlide(_ sender: UIButton) {
+            self.timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(updateTimer(_:)), userInfo: nil, repeats: true)
 
+    }
+    
+    @IBAction func tapped(sender: Any) {
+        if flg {
+            imageview.image = UIImage(named: imageArray[index])
+            flg = false
+        }
+        else {
+            imageview.image = UIImage(named: imageArray[index])
+            flg = true
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
